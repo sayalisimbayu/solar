b074e631975b3828b100947003e419f03774f49d
Amar Barge <Amar.Barge@gep.com>
2020-09-14 10:44:07 +0530

    reset password bug solved

--- src/app/authentication/page-forgot-password/page-forgot-password.component.ts
+++ src/app/authentication/page-forgot-password/page-forgot-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@app/shell/auth/auth.service';
+import { resetUser } from '@app/shell/models/user.model';
 
 @Component({
   selector: 'app-page-forgot-password',
@@ -8,17 +9,21 @@ import { AuthService } from '@app/shell/auth/auth.service';
   styleUrls: ['./page-forgot-password.component.css']
 })
 export class PageForgotPasswordComponent implements OnInit {
-  public user: any = { email: '', otp: '' };
+  public user: resetUser = {
+    email: '',
+    otp: ''
+  };
   public isForgot = true;
-  constructor(private router: Router, private authSvc: AuthService) {}
+  constructor(private router: Router, private authSvc: AuthService) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
   forgot() {
     this.authSvc.forgot(this.user.email).subscribe(res => {
       this.isForgot = !this.isForgot;
     });
   }
   onSubmit() {
+    debugger;
     this.authSvc.reset(this.user).subscribe(res => {
       if (res) {
         this.router.navigate(['/authentication/page-login']);
