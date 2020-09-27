c6246982337780b8bfe1f9e3693add8e0bfa807e
Sayali Nazare <sayali.nazare@gep.com>
2020-09-27 09:53:22 +0530

    practise

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -17,6 +17,10 @@ export const routes: Routes = [
   {
     path: 'authentication',
     loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
+  },
+  {
+    path: 'authentication',
+    loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
   }
 ];
 
