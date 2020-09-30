638721654a9bfd17e1a8ddf7370cd7aaea82cc7e
Sayali Nazare <sayali.nazare@gep.com>
2020-09-30 14:28:11 +0530

    some changes

--- src/app/shell/auth/auth.service.ts
+++ src/app/shell/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject } from '@Angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as moment from 'moment';
 import { Observable } from 'rxjs';
